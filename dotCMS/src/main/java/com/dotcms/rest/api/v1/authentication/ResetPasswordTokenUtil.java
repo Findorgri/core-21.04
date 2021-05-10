a5158e9d40f8291294e6712d7cb22bf328b27600
Christos Mathas <mathas.ch.m@gmail.com>
2021-05-10 13:24:27 +0300

    First commit

---
+++ dotCMS/src/main/java/com/dotcms/rest/api/v1/authentication/ResetPasswordTokenUtil.java
@@ -0,0 +1,61 @@
+package com.dotcms.rest.api.v1.authentication;
+
+import com.dotmarketing.util.Config;
+import com.dotmarketing.util.UtilMethods;
+import com.liferay.portal.model.User;
+import com.liferay.util.StringPool;
+import java.util.Calendar;
+import java.util.Date;
+import org.apache.commons.lang.RandomStringUtils;
+
+/**
+ * Util to manage the token that is used in the user reset password proccess.
+ */
+public class ResetPasswordTokenUtil {
+
+    /**
+     * Check if a token is a valid token.
+     * there are two reasons why a token could be:
+     * <ul>
+     *     <li>The token has a wrong syntax, the token have to match the follow regular expression:
+     *     <b>^[a-zA-Z0-9]+:[0-9]+$</b></li>
+     *     <li>The token expired, the token has a expiration time of 20 minutes by default but it could be
+     *     overwrite setting the RECOVER_PASSWORD_TOKEN_TTL_MINS properties</li>
+     * </ul>
+     *
+     * @param user for who the token is check
+     * @param token to be check
+     * @throws DotInvalidTokenException if the token is invalid, if the token expired then the expired properties
+     *          is set to true
+     */
+    public static void checkToken(final User user, final String token) throws DotInvalidTokenException {
+        final String userIcqId = user.getIcqId();
+        if(UtilMethods.isSet(userIcqId) && userIcqId.matches("^[a-zA-Z0-9]+:[0-9]+$") && userIcqId.equals(token) && UtilMethods.isSet(token)) {
+                // check if token expired
+                Calendar ttl = Calendar.getInstance();
+                ttl.setTimeInMillis(Long.parseLong(userIcqId.substring(userIcqId.indexOf(':')+1)));
+                ttl.add(Calendar.MINUTE, Config.getIntProperty("RECOVER_PASSWORD_TOKEN_TTL_MINS", 20));
+                if(!ttl.after(Calendar.getInstance())) {
+                    throw new DotInvalidTokenException(userIcqId, true);
+                }
+        }else{
+            throw new DotInvalidTokenException(userIcqId);
+        }
+    }
+
+    /**
+     * Create a token to be used in the user reset password process.
+     *
+     * The token has the follow syntax: <Random alphanumeric characters>:<currently timestamp>
+     *
+     * The <b>Random alphanumeric characters</b> part has a default length of 30 characters but a different
+     * value could be define setting the RECOVER_PASSWORD_TOKEN_LENGTH properties.
+     *
+     * @return a newly token
+     */
+    public static String createToken(){
+        return RandomStringUtils.randomAlphanumeric( Config.getIntProperty( "RECOVER_PASSWORD_TOKEN_LENGTH", 30 ) )
+                + StringPool.COLON + new Date().getTime();
+    }
+
+}
---
+++ dotCMS/src/main/java/com/dotmarketing/portlets/contentlet/business/exporter/ImageFilterExporter.java
@@ -0,0 +1,83 @@
+package com.dotmarketing.portlets.contentlet.business.exporter;
+
+import java.io.File;
+import java.util.Map;
+import java.util.Map.Entry;
+import com.dotmarketing.image.filter.ImageFilter;
+import com.dotmarketing.image.filter.ImageFilterApiImpl;
+import com.dotmarketing.image.filter.PDFImageFilter;
+import com.dotmarketing.portlets.contentlet.business.BinaryContentExporter;
+import com.dotmarketing.portlets.contentlet.business.BinaryContentExporterException;
+import com.dotmarketing.util.Logger;
+import com.dotmarketing.util.UtilMethods;
+
+
+/**
+ * 
+ * A exporter that can take 1 or more filters in a chain
+ * 
+ * the chain is provided by the "filter=" parameter You can chain filters so that you resize then
+ * crop to produce the resulting image
+ * 
+ * 
+ */
+
+public class ImageFilterExporter implements BinaryContentExporter {
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see
+     * com.dotmarketing.portlets.contentlet.business.BinaryContentExporter#exportContent(java.io.File,
+     * java.util.Map)
+     */
+    public BinaryContentExporterData exportContent(File file, final Map<String, String[]> parameters)
+                    throws BinaryContentExporterException {
+
+        BinaryContentExporterData data;
+
+        try {
+
+            final Map<String,Class> filters = new ImageFilterApiImpl().resolveFilters(parameters);
+            parameters.put("filter", filters.keySet().toArray(new String[filters.size()]));
+            parameters.put("filters", filters.keySet().toArray(new String[filters.size()]));
+            
+            // run pdf filter first (if a pdf)
+            if(!filters.isEmpty() && "pdf".equals(UtilMethods.getFileExtension(file.getName())) && !filters.containsKey("pdf")) {
+                file = new PDFImageFilter().runFilter(file, parameters);
+            }
+            
+            for (final Entry<String, Class> filter : filters.entrySet()) {
+                try {
+
+                    final ImageFilter imageFilter = (ImageFilter) filter.getValue().newInstance();
+                    file = imageFilter.runFilter(file, parameters);
+                } catch (Exception e) {
+                    Logger.warnAndDebug(ImageFilterExporter.class,
+                                    "Exception in " + filter + " :" + e.getMessage() + e.getStackTrace()[0], e);
+                }
+            }
+
+            data = new BinaryContentExporterData(file);
+        } catch (Exception e) {
+
+            Logger.warnAndDebug(ImageFilterExporter.class,  e);
+            throw new BinaryContentExporterException(e.getMessage(), e);
+        }
+
+        return data;
+    }
+
+    public String getName() {
+        return "Image Filter Exporter";
+    }
+
+    public String getPathMapping() {
+        return "image";
+    }
+
+    public String getDescription() {
+        return "Specify filters to run a source image through";
+    }
+
+}
